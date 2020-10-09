0779a298804ada264b8ed4e3f5ff7685fc9b9a6c
Yuliya-Nad <nad.yul2002@gmail.com>
2020-10-09 22:21:18 +0300

    Added an option to set upper and bottom bounds of graphs and comments as well

--- MainPlot.py
+++ MainPlot.py
@@ -5,44 +5,37 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 class MainDash:
-    df = pd.read_csv('OOP_\\BTC__USD.csv', usecols=['Close', 'Date', 'Volume_BTC'])[::-1]
+    df = pd.read_csv('OOP_\\BTC__USD.csv', usecols=['Close', 'Date', 'Volume_BTC', 'Volume_USD'])
+
     def __init__(self, bt_bound, up_bound):
-        self.bt_bound = bt_bound
-        self.up_bound = up_bound
-        self.df = self.df.iloc[self.bt_bound : self.up_bound]
-    def drawLineDash(self):
-        fig = px.line(x = self.df.Date[::-1], y = self.df.Close[::-1])
+        #self.bt_bound = bt_bound
+        #self.up_bound = up_bound
+        filter1 = self.df[self.df.Date == up_bound].index[0]
+        filter2 = self.df[self.df.Date == bt_bound].index[0]
+        self.df = self.df.iloc[filter1-1 : filter2][::-1] #get an appropriate part of table
+
+    def drawLineDash(self): #draw a line-graph
+        fig = px.line(x = self.df.Date[::-1], y = self.df.Close[::-1]) #x:date; y:price
         fig.show()
 
     def bubblePlot(self):
-        fig = go.Figure(data=[go.Scatter(x = self.df.Close[::-1], y = self.df.Date[::-1],
+        fig = go.Figure(data=[go.Scatter(x = self.df.Date[::-1], y = self.df.Close[::-1],
                                          mode = 'markers',
                                          marker = dict(color = self.df.Volume_BTC/200 ,
                                                        size = self.df.Volume_BTC/100,
                                                        showscale = True))])
-        fig.show()
+        fig.show() #x:Date; y:Price; color & size:Volume
 
     def mixing(self):
         fig = px.bar(self.df, y='Close', x=self.df.Date, color='Volume_BTC', labels={'y': 'Close'},
-                     hover_data=['Close'],
+                     hover_data=['Volume_BTC', 'Volume_USD'],
                      title='Evolution')
-        fig.show()
+        fig.show() #x:Date; y:Price; color:Volume; prompted_data:'Volume_BTC', 'Volume_USD'
 
-graph = MainDash(10, 80)
+graph = MainDash("2018-09-27", "2019-03-27")
 graph.drawLineDash()
 graph.bubblePlot()
 graph.mixing()
 
-# Bar Graph for DataFrame
-# fig = go.Figure(data=go.Bar(x = list(df.Date), y=list(df.Price)))
-# fig.show()
-
-# Table with graphics
-# fig = go.Figure(data=[go.Table(header=dict(values=list(df.columns),
-#                 fill_color='paleturquoise',align='left'),
-#                 cells=dict(values=[df.Date, df.Price],
-#                 fill_color='lavender', align='left'))
-# ])
-# fig.show()
 
 
