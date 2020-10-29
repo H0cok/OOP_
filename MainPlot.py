44d89ad64e23fc8c0be40781ac3eb47d50acd1d6
Yuliya-Nad <nad.yul2002@gmail.com>
2020-10-29 19:45:09 +0200

    It predicts and it should predict better

--- MainPlot.py
+++ MainPlot.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import os
 import plotly.express as px
-import plotly.graph_objects as go
 import datetime
+import numpy as np
+from statsmodels.tsa.arima_model import ARIMA
+import warnings
+warnings.filterwarnings("ignore")
 
 if not os.path.exists("OOP_\\images"):
     os.mkdir("OOP_\\images")
@@ -34,22 +37,21 @@ class UserInput:
     def setState(self, state_inp): #triggered by generate btn
         self.state = state_inp
 
-basic_input = UserInput(datetime.date(2016, 23, 4), datetime.date(2016, 23, 9), "BTC", "day")
+basic_input = UserInput(datetime.date(2016, 4, 23), datetime.date(2016, 9, 23), "BTC", "day")
 
-class MainDash:
+class Plot:
     df = pd.read_csv('OOP_\\BTC__USD.csv', usecols=['Close', 'Date', 'Volume_BTC', 'Volume_USD'])
 
     def __init__(self, bt_bound, up_bound):
-        #self.bt_bound = bt_bound
-        #self.up_bound = up_bound
         filter1 = self.df[self.df.Date == up_bound].index[0]
         filter2 = self.df[self.df.Date == bt_bound].index[0]
         self.df = self.df.iloc[filter1-1 : filter2][::-1] #get an appropriate part of table
 
-    def drawLineDash(self): #draw a line-graph
-        fig = px.line(x = self.df.Date[::-1], y = self.df.Close[::-1]) #x:date; y:price
+    def drawPlot(self): #draw a line based plot
+        fig = px.line(self.df, x = 'Date', y = 'Close') #x:date; y:price
         fig.show()
-        #fig.write_image("OOP_\\images\\fig1.png")
+        fig.to_image(format="png", engine="kaleido")
+        fig.write_image("OOP_\\images\\fig1.png")
 
     def mixing(self):
         fig = px.bar(self.df, y='Close', x=self.df.Date, color='Volume_BTC', labels={'y': 'Close'},
@@ -58,9 +60,35 @@ class MainDash:
         fig.show() #x:Date; y:Price; color:Volume; prompted_data:'Volume_BTC', 'Volume_USD'
         #fig.write_image("OOP_\\images\\fig3.png", engine = 'kaleido')
 
-graph = MainDash("2018-09-27", "2019-03-27")
-graph.drawLineDash()
-graph.mixing()
+graph = Plot("2018-09-27", "2019-03-27")
+#graph.drawPlot()
+#graph.mixing()
 
+class Genie:
+    # def best_model(self, df):
+    #     self.ad_test(df['Close'])
+    #     wise_step = auto_arima(df['Close'], trace = True, suppress_warnings = True)
+    #     wise_step.summary()
 
+    def predict_val(self):
+        df = pd.read_csv('OOP_\\BTC__USD.csv', usecols=['Close', 'Date', 'Volume_BTC', 'Volume_USD'], parse_dates=True)
+        df = df.dropna()#discard extra columns
+        df = df.iloc[::-1]#reverse data-frame
 
+        #divide to test and train sections
+        train = df.iloc[:-30]
+        test = df.iloc[-30:]
+
+        model = ARIMA(train['Close'], order=(1,1,0)) #order use best-model
+        model = model.fit()
+        start = len(train)
+        end = len(train)+len(test)-1
+        pred = model.predict(start=start, end=end, typ = 'levels')
+        pred = pred.to_list()
+        test['Predicted'] = pred
+        print(test)
+        fig = px.line(test, x = 'Date', y = 'Predicted') #x:date; y:price
+        fig.show()
+
+prediction = Genie()
+prediction.predict_val()
