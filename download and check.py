9dc1a178b8a7f6255d1885c88bedd0907eb95dab
Yuliya-Nad <nad.yul2002@gmail.com>
2020-11-27 00:22:42 +0200

    regression for future

--- download and check.py
+++ download and check.py
@@ -6,8 +6,7 @@ import datetime
 import numpy as np
 import plotly.express as px
 from statsmodels.tsa.arima_model import ARIMA
-from sklearn.preprocessing import MinMaxScaler
-#dependencies for the last method
+#dependencies for the DecisionTree method
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 
@@ -126,18 +125,15 @@ class Plot:
 class Genie:
     def predict_ml(self, df):
         df = df.iloc[::-1] # reverse data-frame
-        dp = df.copy()
         df = df[['Close']]
         future_days = 30 # variable to predict 'x' days
         df['Prediction'] = df[['Close']].shift(-future_days)
 
         #creating a feature data set converted to numpy array without the last 'x' rows
         x = np.array(df.drop(['Prediction'], 1))[:-future_days]
-        #creating the target data set
         y = np.array(df['Prediction'])[:-future_days]
-        #Split the data 70% training and 30% testing
-        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
-        #The decision tree regressor model
+        #Split the data for training and testing
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1)
         tree = DecisionTreeRegressor().fit(x_train, y_train)
 
         #Get the last 'x' rows from the feature data set
@@ -146,19 +142,19 @@ class Genie:
         x_future = np.array(x_future)
         #Show the model tree prediction
         tree_prediction = tree.predict(x_future)
-        # print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, tree_prediction))
-        # print('Mean Squared Error:', metrics.mean_squared_error(y_test, tree_prediction))
         test = pd.DataFrame()
         test['Tree'] = tree_prediction
-        date = np.array(dp['Date'][-30:])
-        test['Date'] = date
+        #Create a current range
+        start = datetime.date.today()
+        date_generated = [start + datetime.timedelta(days=x) for x in range(future_days)]
+        date_table = []
+        for date in date_generated:
+            date_table.append(date.strftime("%Y-%m-%d"))
+        test['Date'] = date_table
+        print(test)
         #Visualizing the data
         fig_p = px.line(test, x = 'Date', y = 'Tree') #x:date; y:price
         fig_p.show()
-        #dp = dp.iloc[-30:]
-        #fig = px.line(dp, x = 'Date', y = 'Close')
-        #fig.show()
-
 
     def predict_val_arima(self, df):
         df = df.iloc[::-1]#reverse data-frame
